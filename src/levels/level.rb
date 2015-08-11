69f212c7bda7d9087deb8fbcf12ec6a2f8ebf674
Nicholas Iannone <nickiannone@gmail.com>
2015-08-11 16:42:35 -0500

    Create level.rb

---
+++ src/levels/level.rb
@@ -0,0 +1,35 @@
+require 'gosu'
+require 'gamewindow'
+
+module Levels
+	class Level
+		def initialize()
+		end
+		
+		# Game callbacks
+		def set_up(gamewindow)
+		end
+		
+		def tear_down(gamewindow)
+		end
+		
+		# Gosu callbacks
+		def draw(gamewindow)
+		end
+		
+		def update(gamewindow)
+		end
+		
+		def button_down(gamewindow, id)
+		end
+		
+		def button_up(gamewindow, id)
+		end
+		
+		def needs_cursor?
+		end
+		
+		def needs_redraw?
+		end
+	end
+end
