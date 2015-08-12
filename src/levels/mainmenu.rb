9c734e7755553589e38f044645900fe4c457a3e5
nickiannone <nickiannone@gmail.com>
2015-08-12 00:09:09 -0500

    Added a bit more to the main menu level class. Need to come up with a better name.

--- src/levels/mainmenu.rb
+++ src/levels/mainmenu.rb
@@ -4,5 +4,41 @@ require 'levels/level'
 module Levels
 	class MainMenu < Level
 		# TODO Implement!
+		def initialize
+		  
+		end
+		
+		# Game callbacks
+    def set_up(gamewindow)
+      
+    end
+    
+    def tear_down(gamewindow)
+    end
+    
+    # Gosu callbacks
+    def draw(gamewindow)
+      # TODO draw the main menu buttons
+    end
+    
+    def update(gamewindow)
+      # TODO pump animation into main menu
+    end
+    
+    def button_down(gamewindow, id)
+      # TODO pump mouse/keyboard input into main menu
+    end
+    
+    def button_up(gamewindow, id)
+      # TODO pump mouse/keyboard input into main menu
+    end
+    
+    def needs_cursor?
+      true
+    end
+    
+    def needs_redraw?
+      # TODO return the main menu's redraw
+    end
 	end
 end
